b51a3779fe4e766a9d821d61c27210467933dc58
Josh Bassett <jbassett9@gmail.com>
2015-07-17 16:24:53 -0400

    fix ready function to change last h2 heading using jq

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 $ (document).ready(function() {
-    // ***Don't understand what I'm doing wrong, but this isn't working***
-
-    //var headings = $( "h2" );
-    //$( "headings:last" ).html( "I'm Josh Bassett, and I'm learning jQuery!" );
-
-    $( "h2:last" ).html( "I'm Josh Bassett, and I'm learning jQuery!" );
+    var headings = $( "h2" );
+    headings.last().html( "I'm Josh Bassett, and I'm learning jQuery!" );
 });
