9b27888384b1bd8add749b8140a3be50e211fcce
Josh Bassett <jbassett9@gmail.com>
2015-07-17 15:00:42 -0400

    use jquery to change last h2 in html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $ (document).ready(function() {
+    // ***Don't understand what I'm doing wrong, but this isn't working***
 
-	$ ('h2').css('color','red');
+    //var headings = $( "h2" );
+    //$( "headings:last" ).html( "I'm Josh Bassett, and I'm learning jQuery!" );
+
+    $( "h2:last" ).html( "I'm Josh Bassett, and I'm learning jQuery!" );
 });
